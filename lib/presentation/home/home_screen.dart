1a0e60d181b294189b8b6d31857d8cc916c2d998
Tigran <tigran.hayrapetyan17ht@gmail.com>
2024-08-19 19:21:40 +0400

    wip:gpt prompt

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
 import 'dart:developer';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:majestica_ds/majestica_ds.dart';
 
@@ -27,26 +27,31 @@ class HomeScreen extends StatelessWidget {
             child: PrimaryButton(
               onPressed: () async {
                 final request = ChatCompleteText(
-                    messages: [
-                      Messages(
-                        role: Role.user,
-                        content:
-                            "Based on my favorite movies 'Inception,' 'The Matrix,','Interstellar,' and my mood being 'Excited,' recommend some movies I might enjoy, including cover image URLs. in json format",
-                      ).toJson(),
-                    ],
-                    maxToken: 200,
-                    model: ChatModelFromValue(model: 'gpt-4o-mini'));
+                  messages: [
+                    Messages(
+                      role: Role.user,
+                      content: '''
+Task: Recommend a movie title based on the user’s  current mood,  preferred genres and favorite movies,.
+Parameters:
+	•	Current Mood: Need a laugh
+	•	Preferred Genres: [Romance,Comedy ]
+  •	Favorite Movies: []
+Instructions: Return a single movie title that aligns with the user’s favorite movies, current mood, and preferred genres. Provide only the movie title. and it should not be on the list anymore''',
+                    ).toJson(),
+                  ],
+                  maxToken: 200,
+                  model: ChatModelFromValue(model: 'gpt-4o-mini'),
+                );
 
                 final response =
                     await openAI.onChatCompletion(request: request);
 
-                log(response!.toJson().toString());
+                print(response!.choices.first.message!.content);
 
-                for (var element in response!.choices) {
-                  print("data -> ${element.message?.content}");
-                }
+                MovieService()
+                    .fetchMovie(response!.choices.first.message!.content);
               },
-              text: 'Text',
+              text: 'Test',
             ),
           )
         ],
@@ -57,3 +62,14 @@ class HomeScreen extends StatelessWidget {
 
 final token =
     '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';
+
+class MovieService {
+  final String apiKey = '84d0e788'; // Replace with your API key
+
+  void fetchMovie(query) async {
+    final response = await Dio()
+        .get('http://www.omdbapi.com/?t=$query&apikey=$apiKey&type=movie');
+
+    print(response);
+  }
+}
