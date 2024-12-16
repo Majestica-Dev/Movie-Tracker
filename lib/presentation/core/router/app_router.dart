132ad1c5beeca284acbb6230b5f7fd5eb9290cc1
sevvak <sevakbaghryan2006@gmail.com>
2024-12-16 08:35:25 +0400

    removed black friday paywall

--- lib/presentation/core/router/app_router.dart
+++ lib/presentation/core/router/app_router.dart
@@ -25,10 +25,5 @@ class AppRouter extends RootStackRouter {
         AutoRoute(page: MovieDetailsRoute.page),
         AutoRoute(page: MovieOverviewRoute.page),
         AutoRoute(page: FavoriteMoviesChooserRoute.page),
-        CustomRoute(
-          page: BlackFridayPaywallRoute.page,
-          transitionsBuilder: TransitionsBuilders.noTransition,
-          reverseDurationInMilliseconds: 0,
-        ),
       ];
 }
--- lib/presentation/core/router/app_router.gr.dart
+++ lib/presentation/core/router/app_router.gr.dart
@@ -8,37 +8,35 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i12;
-import 'package:flutter/cupertino.dart' as _i14;
-import 'package:flutter/material.dart' as _i13;
+import 'package:auto_route/auto_route.dart' as _i11;
+import 'package:flutter/cupertino.dart' as _i12;
+import 'package:flutter/material.dart' as _i16;
 import 'package:movie_tracker/application/movie/ai_rec/form/movie_ai_rec_form_cubit.dart'
-    as _i17;
+    as _i15;
 import 'package:movie_tracker/domain/movie/entities/ai_rec/watch_status.dart'
-    as _i16;
-import 'package:movie_tracker/domain/movie/entities/movie.dart' as _i15;
+    as _i14;
+import 'package:movie_tracker/domain/movie/entities/movie.dart' as _i13;
 import 'package:movie_tracker/presentation/base/base_screen.dart' as _i1;
 import 'package:movie_tracker/presentation/discover/discover_screen.dart'
-    as _i3;
-import 'package:movie_tracker/presentation/home/home_screen.dart' as _i5;
-import 'package:movie_tracker/presentation/initial/initial_view.dart' as _i6;
-import 'package:movie_tracker/presentation/intro/intro_screen.dart' as _i7;
-import 'package:movie_tracker/presentation/movie_details/details_screen/movie_details_screen.dart'
-    as _i8;
-import 'package:movie_tracker/presentation/movie_details/overwiew/movie_overview_screen.dart'
-    as _i9;
-import 'package:movie_tracker/presentation/onboarding/favorite_movies/favorite_movies_chooser_screen.dart'
-    as _i4;
-import 'package:movie_tracker/presentation/onboarding/onboarding_screen.dart'
-    as _i10;
-import 'package:movie_tracker/presentation/onboarding/paywall/black_friday/black_friday_paywall_screen.dart'
     as _i2;
+import 'package:movie_tracker/presentation/home/home_screen.dart' as _i4;
+import 'package:movie_tracker/presentation/initial/initial_view.dart' as _i5;
+import 'package:movie_tracker/presentation/intro/intro_screen.dart' as _i6;
+import 'package:movie_tracker/presentation/movie_details/details_screen/movie_details_screen.dart'
+    as _i7;
+import 'package:movie_tracker/presentation/movie_details/overwiew/movie_overview_screen.dart'
+    as _i8;
+import 'package:movie_tracker/presentation/onboarding/favorite_movies/favorite_movies_chooser_screen.dart'
+    as _i3;
+import 'package:movie_tracker/presentation/onboarding/onboarding_screen.dart'
+    as _i9;
 import 'package:movie_tracker/presentation/settings/settings_screen.dart'
-    as _i11;
+    as _i10;
 
 /// generated route for
 /// [_i1.BaseScreen]
-class BaseRoute extends _i12.PageRouteInfo<void> {
-  const BaseRoute({List<_i12.PageRouteInfo>? children})
+class BaseRoute extends _i11.PageRouteInfo<void> {
+  const BaseRoute({List<_i11.PageRouteInfo>? children})
       : super(
           BaseRoute.name,
           initialChildren: children,
@@ -46,7 +44,7 @@ class BaseRoute extends _i12.PageRouteInfo<void> {
 
   static const String name = 'BaseRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
       return const _i1.BaseScreen();
@@ -55,56 +53,9 @@ class BaseRoute extends _i12.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i2.BlackFridayPaywallScreen]
-class BlackFridayPaywallRoute
-    extends _i12.PageRouteInfo<BlackFridayPaywallRouteArgs> {
-  BlackFridayPaywallRoute({
-    required bool isFromOnboarding,
-    _i13.Key? key,
-    List<_i12.PageRouteInfo>? children,
-  }) : super(
-          BlackFridayPaywallRoute.name,
-          args: BlackFridayPaywallRouteArgs(
-            isFromOnboarding: isFromOnboarding,
-            key: key,
-          ),
-          initialChildren: children,
-        );
-
-  static const String name = 'BlackFridayPaywallRoute';
-
-  static _i12.PageInfo page = _i12.PageInfo(
-    name,
-    builder: (data) {
-      final args = data.argsAs<BlackFridayPaywallRouteArgs>();
-      return _i2.BlackFridayPaywallScreen(
-        isFromOnboarding: args.isFromOnboarding,
-        key: args.key,
-      );
-    },
-  );
-}
-
-class BlackFridayPaywallRouteArgs {
-  const BlackFridayPaywallRouteArgs({
-    required this.isFromOnboarding,
-    this.key,
-  });
-
-  final bool isFromOnboarding;
-
-  final _i13.Key? key;
-
-  @override
-  String toString() {
-    return 'BlackFridayPaywallRouteArgs{isFromOnboarding: $isFromOnboarding, key: $key}';
-  }
-}
-
-/// generated route for
-/// [_i3.DiscoverScreen]
-class DiscoverRoute extends _i12.PageRouteInfo<void> {
-  const DiscoverRoute({List<_i12.PageRouteInfo>? children})
+/// [_i2.DiscoverScreen]
+class DiscoverRoute extends _i11.PageRouteInfo<void> {
+  const DiscoverRoute({List<_i11.PageRouteInfo>? children})
       : super(
           DiscoverRoute.name,
           initialChildren: children,
@@ -112,18 +63,18 @@ class DiscoverRoute extends _i12.PageRouteInfo<void> {
 
   static const String name = 'DiscoverRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
-      return const _i3.DiscoverScreen();
+      return const _i2.DiscoverScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i4.FavoriteMoviesChooserScreen]
-class FavoriteMoviesChooserRoute extends _i12.PageRouteInfo<void> {
-  const FavoriteMoviesChooserRoute({List<_i12.PageRouteInfo>? children})
+/// [_i3.FavoriteMoviesChooserScreen]
+class FavoriteMoviesChooserRoute extends _i11.PageRouteInfo<void> {
+  const FavoriteMoviesChooserRoute({List<_i11.PageRouteInfo>? children})
       : super(
           FavoriteMoviesChooserRoute.name,
           initialChildren: children,
@@ -131,21 +82,21 @@ class FavoriteMoviesChooserRoute extends _i12.PageRouteInfo<void> {
 
   static const String name = 'FavoriteMoviesChooserRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
-      return const _i4.FavoriteMoviesChooserScreen();
+      return const _i3.FavoriteMoviesChooserScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i5.HomeScreen]
-class HomeRoute extends _i12.PageRouteInfo<HomeRouteArgs> {
+/// [_i4.HomeScreen]
+class HomeRoute extends _i11.PageRouteInfo<HomeRouteArgs> {
   HomeRoute({
     required bool isFromOnboarding,
-    _i14.Key? key,
-    List<_i12.PageRouteInfo>? children,
+    _i12.Key? key,
+    List<_i11.PageRouteInfo>? children,
   }) : super(
           HomeRoute.name,
           args: HomeRouteArgs(
@@ -157,11 +108,11 @@ class HomeRoute extends _i12.PageRouteInfo<HomeRouteArgs> {
 
   static const String name = 'HomeRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
       final args = data.argsAs<HomeRouteArgs>();
-      return _i5.HomeScreen(
+      return _i4.HomeScreen(
         isFromOnboarding: args.isFromOnboarding,
         key: args.key,
       );
@@ -177,7 +128,7 @@ class HomeRouteArgs {
 
   final bool isFromOnboarding;
 
-  final _i14.Key? key;
+  final _i12.Key? key;
 
   @override
   String toString() {
@@ -186,9 +137,9 @@ class HomeRouteArgs {
 }
 
 /// generated route for
-/// [_i6.InitialScreen]
-class InitialRoute extends _i12.PageRouteInfo<void> {
-  const InitialRoute({List<_i12.PageRouteInfo>? children})
+/// [_i5.InitialScreen]
+class InitialRoute extends _i11.PageRouteInfo<void> {
+  const InitialRoute({List<_i11.PageRouteInfo>? children})
       : super(
           InitialRoute.name,
           initialChildren: children,
@@ -196,18 +147,18 @@ class InitialRoute extends _i12.PageRouteInfo<void> {
 
   static const String name = 'InitialRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
-      return const _i6.InitialScreen();
+      return const _i5.InitialScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i7.IntroScreen]
-class IntroRoute extends _i12.PageRouteInfo<void> {
-  const IntroRoute({List<_i12.PageRouteInfo>? children})
+/// [_i6.IntroScreen]
+class IntroRoute extends _i11.PageRouteInfo<void> {
+  const IntroRoute({List<_i11.PageRouteInfo>? children})
       : super(
           IntroRoute.name,
           initialChildren: children,
@@ -215,21 +166,21 @@ class IntroRoute extends _i12.PageRouteInfo<void> {
 
   static const String name = 'IntroRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
-      return const _i7.IntroScreen();
+      return const _i6.IntroScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i8.MovieDetailsScreen]
-class MovieDetailsRoute extends _i12.PageRouteInfo<MovieDetailsRouteArgs> {
+/// [_i7.MovieDetailsScreen]
+class MovieDetailsRoute extends _i11.PageRouteInfo<MovieDetailsRouteArgs> {
   MovieDetailsRoute({
     required String id,
-    _i14.Key? key,
-    List<_i12.PageRouteInfo>? children,
+    _i12.Key? key,
+    List<_i11.PageRouteInfo>? children,
   }) : super(
           MovieDetailsRoute.name,
           args: MovieDetailsRouteArgs(
@@ -241,11 +192,11 @@ class MovieDetailsRoute extends _i12.PageRouteInfo<MovieDetailsRouteArgs> {
 
   static const String name = 'MovieDetailsRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
       final args = data.argsAs<MovieDetailsRouteArgs>();
-      return _i8.MovieDetailsScreen(
+      return _i7.MovieDetailsScreen(
         id: args.id,
         key: args.key,
       );
@@ -261,7 +212,7 @@ class MovieDetailsRouteArgs {
 
   final String id;
 
-  final _i14.Key? key;
+  final _i12.Key? key;
 
   @override
   String toString() {
@@ -270,16 +221,16 @@ class MovieDetailsRouteArgs {
 }
 
 /// generated route for
-/// [_i9.MovieOverviewScreen]
-class MovieOverviewRoute extends _i12.PageRouteInfo<MovieOverviewRouteArgs> {
+/// [_i8.MovieOverviewScreen]
+class MovieOverviewRoute extends _i11.PageRouteInfo<MovieOverviewRouteArgs> {
   MovieOverviewRoute({
-    required _i15.Movie movie,
+    required _i13.Movie movie,
     required bool isFromAi,
-    required _i16.WatchStatus? watchStatus,
-    required _i17.MovieAiRecFormState? movieAiRecFormState,
+    required _i14.WatchStatus? watchStatus,
+    required _i15.MovieAiRecFormState? movieAiRecFormState,
     required bool isFavorite,
-    _i13.Key? key,
-    List<_i12.PageRouteInfo>? children,
+    _i16.Key? key,
+    List<_i11.PageRouteInfo>? children,
   }) : super(
           MovieOverviewRoute.name,
           args: MovieOverviewRouteArgs(
@@ -295,11 +246,11 @@ class MovieOverviewRoute extends _i12.PageRouteInfo<MovieOverviewRouteArgs> {
 
   static const String name = 'MovieOverviewRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
       final args = data.argsAs<MovieOverviewRouteArgs>();
-      return _i9.MovieOverviewScreen(
+      return _i8.MovieOverviewScreen(
         movie: args.movie,
         isFromAi: args.isFromAi,
         watchStatus: args.watchStatus,
@@ -321,17 +272,17 @@ class MovieOverviewRouteArgs {
     this.key,
   });
 
-  final _i15.Movie movie;
+  final _i13.Movie movie;
 
   final bool isFromAi;
 
-  final _i16.WatchStatus? watchStatus;
+  final _i14.WatchStatus? watchStatus;
 
-  final _i17.MovieAiRecFormState? movieAiRecFormState;
+  final _i15.MovieAiRecFormState? movieAiRecFormState;
 
   final bool isFavorite;
 
-  final _i13.Key? key;
+  final _i16.Key? key;
 
   @override
   String toString() {
@@ -340,9 +291,9 @@ class MovieOverviewRouteArgs {
 }
 
 /// generated route for
-/// [_i10.OnboardingScreen]
-class OnboardingRoute extends _i12.PageRouteInfo<void> {
-  const OnboardingRoute({List<_i12.PageRouteInfo>? children})
+/// [_i9.OnboardingScreen]
+class OnboardingRoute extends _i11.PageRouteInfo<void> {
+  const OnboardingRoute({List<_i11.PageRouteInfo>? children})
       : super(
           OnboardingRoute.name,
           initialChildren: children,
@@ -350,18 +301,18 @@ class OnboardingRoute extends _i12.PageRouteInfo<void> {
 
   static const String name = 'OnboardingRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
-      return const _i10.OnboardingScreen();
+      return const _i9.OnboardingScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i11.SettingsScreen]
-class SettingsRoute extends _i12.PageRouteInfo<void> {
-  const SettingsRoute({List<_i12.PageRouteInfo>? children})
+/// [_i10.SettingsScreen]
+class SettingsRoute extends _i11.PageRouteInfo<void> {
+  const SettingsRoute({List<_i11.PageRouteInfo>? children})
       : super(
           SettingsRoute.name,
           initialChildren: children,
@@ -369,10 +320,10 @@ class SettingsRoute extends _i12.PageRouteInfo<void> {
 
   static const String name = 'SettingsRoute';
 
-  static _i12.PageInfo page = _i12.PageInfo(
+  static _i11.PageInfo page = _i11.PageInfo(
     name,
     builder: (data) {
-      return const _i11.SettingsScreen();
+      return const _i10.SettingsScreen();
     },
   );
 }
